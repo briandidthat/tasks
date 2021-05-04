3e15171299c1d079b03d60a6dc731f7341acbffc
brianthf <blora616@gmail.com>
2021-05-04 15:52:01 -0400

    fix domain

--- src/main/java/com/organicautonomy/tasks/domain/Task.java
+++ src/main/java/com/organicautonomy/tasks/domain/Task.java
@@ -2,6 +2,7 @@ package com.organicautonomy.tasks.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
